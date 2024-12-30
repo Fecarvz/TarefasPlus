0f32fffc0080ca32482aff8e4271c7b3965dcf6e
Felipe Carvalho <102259210+Fecarvz@users.noreply.github.com>
2024-12-30 14:30:50 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyC7k6uUWG97ltsc5uSs9BYPY3O2xUbX57U",
-  authDomain: "tarefasplus-3f3c2.firebaseapp.com",
-  projectId: "tarefasplus-3f3c2",
-  storageBucket: "tarefasplus-3f3c2.firebasestorage.app",
-  messagingSenderId: "861249037674",
-  appId: "1:861249037674:web:905b779a749f8ff1d93113",
-  measurementId: "G-CQQ4LMQ0Z3"
+  apiKey: "***********************",
+  authDomain: ""***********************",
+  projectId: ""***********************",
+  storageBucket: ""***********************",
+  messagingSenderId: ""***********************",
+  appId: ""***********************",
+  measurementId: ""***********************"
 };
 
 // Initialize Firebase
